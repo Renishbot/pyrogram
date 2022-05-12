342abfcac922f5bbe677443ecadd490a6139be9d
sam01101 <sam+git@0s.hk>
2022-05-12 16:56:18 +0800

    🚀 Make Pyrogram start faster by default, this did no harm to any code. (This requires you to re-compile raw folder)

--- compiler/api/compiler.py
+++ compiler/api/compiler.py
@@ -21,6 +21,7 @@ import re
 import shutil
 from functools import partial
 from pathlib import Path
+from sys import argv
 from typing import NamedTuple, List, Tuple
 
 # from autoflake import fix_code
@@ -594,9 +595,381 @@ def start(format: bool = False):
         f.write("\n}\n")
 
 
+# noinspection PyShadowingBuiltins
+def start_fast(format: bool = False):
+    shutil.rmtree(DESTINATION_PATH / "types", ignore_errors=True)
+    shutil.rmtree(DESTINATION_PATH / "functions", ignore_errors=True)
+    shutil.rmtree(DESTINATION_PATH / "base", ignore_errors=True)
+
+    with open(HOME_PATH / "source/auth_key.tl") as f1, \
+            open(HOME_PATH / "source/sys_msgs.tl") as f2, \
+            open(HOME_PATH / "source/main_api.tl") as f3:
+        schema = (f1.read() + f2.read() + f3.read()).splitlines()
+
+    with open(HOME_PATH / "template/type_fast.txt") as f1, \
+            open(HOME_PATH / "template/combinator_fast.txt") as f2:
+        type_tmpl = f1.read()
+        combinator_tmpl = f2.read()
+
+    with open(NOTICE_PATH, encoding="utf-8") as f:
+        notice = []
+
+        for line in f.readlines():
+            notice.append(f"#  {line}".strip())
+
+        notice = "\n".join(notice)
+
+    section = None
+    layer = None
+    combinators = []
+    header_inserted = []
+
+    for line in schema:
+        # Check for section changer lines
+        section_match = SECTION_RE.match(line)
+        if section_match:
+            section = section_match.group(1)
+            continue
+
+        # Save the layer version
+        layer_match = LAYER_RE.match(line)
+        if layer_match:
+            layer = layer_match.group(1)
+            continue
+
+        combinator_match = COMBINATOR_RE.match(line)
+        if combinator_match:
+            # noinspection PyShadowingBuiltins
+            qualname, id, qualtype = combinator_match.groups()
+
+            namespace, name = qualname.split(".") if "." in qualname else ("", qualname)
+            name = camel(name)
+            qualname = ".".join([namespace, name]).lstrip(".")
+
+            typespace, type = qualtype.split(".") if "." in qualtype else ("", qualtype)
+            type = camel(type)
+            qualtype = ".".join([typespace, type]).lstrip(".")
+
+            # Pingu!
+            has_flags = not not FLAGS_RE_3.findall(line)
+
+            args = ARGS_RE.findall(line)
+
+            # Fix arg name being "self" (reserved python keyword)
+            for i, item in enumerate(args):
+                if item[0] == "self":
+                    args[i] = ("is_self", item[1])
+
+            combinator = Combinator(
+                section=section,
+                qualname=qualname,
+                namespace=namespace,
+                name=name,
+                id=f"0x{id}",
+                has_flags=has_flags,
+                args=args,
+                qualtype=qualtype,
+                typespace=typespace,
+                type=type
+            )
+
+            combinators.append(combinator)
+
+    for combinator in combinators:
+        qualtype = combinator.qualtype
+
+        if qualtype.startswith("Vector"):
+            qualtype = qualtype.split("<")[1][:-1]
+
+        d = types_to_constructors if combinator.section == "types" else types_to_functions
+
+        if qualtype not in d:
+            d[qualtype] = []
+
+        d[qualtype].append(combinator.qualname)
+
+        if combinator.section == "types":
+            key = combinator.namespace
+
+            if key not in namespaces_to_types:
+                namespaces_to_types[key] = []
+
+            if combinator.type not in namespaces_to_types[key]:
+                namespaces_to_types[key].append(combinator.type)
+
+    for k, v in types_to_constructors.items():
+        for i in v:
+            try:
+                constructors_to_functions[i] = types_to_functions[k]
+            except KeyError:
+                pass
+
+    for qualtype in types_to_constructors:
+        typespace, type = qualtype.split(".") if "." in qualtype else ("", qualtype)
+        dir_path = DESTINATION_PATH / "base"
+
+        module = typespace or '__init__'
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        constructors = sorted(types_to_constructors[qualtype])
+        constr_count = len(constructors)
+        items = "\n            ".join([f"- :obj:`{c} <pyrogram.raw.types.{c}>`" for c in constructors])
+
+        docstring = f"This base type has {constr_count} constructor{'s' if constr_count > 1 else ''} available.\n\n"
+        docstring += f"    Constructors:\n        .. hlist::\n            :columns: 2\n\n            {items}"
+
+        references, ref_count = get_references(qualtype, "types")
+
+        if references:
+            docstring += f"\n\n    See Also:\n        This object can be returned by " \
+                         f"{ref_count} method{'s' if ref_count > 1 else ''}:" \
+                         f"\n\n        .. hlist::\n            :columns: 2\n\n            " + references
+
+        with open(dir_path / f"{module}.py", "a") as f:
+            if module not in header_inserted:
+                header_inserted.append(module)
+                f.write(
+                    f"{notice}\n\n"
+                    f"{WARNING}\n\n"
+                    "from pyrogram.raw.core import TLObject\n\n"
+                )
+            f.write(
+                type_tmpl.format(
+                    # notice=notice,
+                    # warning=WARNING,
+                    docstring=docstring,
+                    name=type,
+                    qualname=qualtype,
+                    # types=", ".join([f"raw.types.{c}" for c in constructors]),
+                    doc_name=snake(type).replace("_", "-")
+                )
+            )
+
+    header_inserted.clear()
+
+    for combinator in combinators:
+        sorted_args = sort_args(combinator.args)
+
+        arguments = (
+                (", *, " if combinator.args else "") +
+                (", ".join(
+                    [f"{i[0]}: {get_type_hint(i[1])}"
+                     for i in sorted_args]
+                ) if sorted_args else "")
+        )
+
+        fields = "\n        ".join(
+            [f"self.{i[0]} = {i[0]}  # {i[1]}"
+             for i in sorted_args]
+        ) if sorted_args else "pass"
+
+        docstring = ""
+        docstring_args = []
+
+        for i, arg in enumerate(sorted_args):
+            arg_name, arg_type = arg
+            is_optional = FLAGS_RE.match(arg_type)
+            flag_number = is_optional.group(1) if is_optional else -1
+            arg_type = arg_type.split("?")[-1]
+
+            docstring_args.append(
+                "{}{}: {}".format(
+                    arg_name,
+                    " (optional)".format(flag_number) if is_optional else "",
+                    get_docstring_arg_type(arg_type, is_pyrogram_type=combinator.namespace == "pyrogram")
+                )
+            )
+
+        if combinator.section == "types":
+            docstring += f"This object is a constructor of the base type :obj:`~pyrogram.raw.base.{combinator.qualtype}`.\n\n"
+        else:
+            docstring += f"Telegram API method.\n\n"
+
+        docstring += f"    Details:\n        - Layer: ``{layer}``\n        - ID: ``{combinator.id[2:].upper()}``\n\n"
+
+        if docstring_args:
+            docstring += "    Parameters:\n        " + "\n        ".join(docstring_args)
+        else:
+            docstring += "    **No parameters required.**"
+
+        if combinator.section == "functions":
+            docstring += "\n\n    Returns:\n        " + get_docstring_arg_type(combinator.qualtype)
+        else:
+            references, count = get_references(combinator.qualname, "constructors")
+
+            if references:
+                docstring += f"\n\n    See Also:\n        This object can be returned by " \
+                             f"{count} method{'s' if count > 1 else ''}:" \
+                             f"\n\n        .. hlist::\n            :columns: 2\n\n            " + references
+
+        write_types = read_types = "" if combinator.has_flags else "# No flags\n        "
+
+        for arg_name, arg_type in combinator.args:
+            flag = FLAGS_RE_2.match(arg_type)
+
+            if re.match(r"flags\d?", arg_name) and arg_type == "#":
+                write_flags = []
+
+                for i in combinator.args:
+                    flag = FLAGS_RE_2.match(i[1])
+
+                    if flag:
+                        if arg_name != f"flags{flag.group(1)}":
+                            continue
+
+                        if flag.group(3) == "true" or flag.group(3).startswith("Vector"):
+                            write_flags.append(f"{arg_name} |= (1 << {flag.group(2)}) if self.{i[0]} else 0")
+                        else:
+                            write_flags.append(
+                                f"{arg_name} |= (1 << {flag.group(2)}) if self.{i[0]} is not None else 0")
+
+                write_flags = "\n        ".join([
+                    f"{arg_name} = 0",
+                    "\n        ".join(write_flags),
+                    f"b.write(Int({arg_name}))\n        "
+                ])
+
+                write_types += write_flags
+                read_types += f"\n        {arg_name} = Int.read(b)\n        "
+
+                continue
+
+            if flag:
+                number, index, flag_type = flag.groups()
+
+                if flag_type == "true":
+                    read_types += "\n        "
+                    read_types += f"{arg_name} = True if flags{number} & (1 << {index}) else False"
+                elif flag_type in CORE_TYPES:
+                    write_types += "\n        "
+                    write_types += f"self.{arg_name} and b.write({flag_type.title()}(self.{arg_name}))\n        "
+
+                    read_types += "\n        "
+                    read_types += f"{arg_name} = {flag_type.title()}.read(b) if flags{number} & (1 << {index}) else None"
+                elif "vector" in flag_type.lower():
+                    sub_type = arg_type.split("<")[1][:-1]
+
+                    write_types += "\n        "
+                    write_types += f"if self.{arg_name}:\n            "
+                    write_types += "b.write(Vector(self.{}{}))\n        ".format(
+                        arg_name, f", {sub_type.title()}" if sub_type in CORE_TYPES else ""
+                    )
+
+                    read_types += "\n        "
+                    read_types += "{} = TLObject.read(b{}) if flags{} & (1 << {}) else []\n        ".format(
+                        arg_name, f", {sub_type.title()}" if sub_type in CORE_TYPES else "", number, index
+                    )
+                else:
+                    write_types += "\n        "
+                    write_types += f"if self.{arg_name} is not None:\n            "
+                    write_types += f"b.write(self.{arg_name}.write())\n        "
+
+                    read_types += "\n        "
+                    read_types += f"{arg_name} = TLObject.read(b) if flags{number} & (1 << {index}) else None\n        "
+            else:
+                if arg_type in CORE_TYPES:
+                    write_types += "\n        "
+                    write_types += f"b.write({arg_type.title()}(self.{arg_name}))\n        "
+
+                    read_types += "\n        "
+                    read_types += f"{arg_name} = {arg_type.title()}.read(b)\n        "
+                elif "vector" in arg_type.lower():
+                    sub_type = arg_type.split("<")[1][:-1]
+
+                    write_types += "\n        "
+                    write_types += "b.write(Vector(self.{}{}))\n        ".format(
+                        arg_name, f", {sub_type.title()}" if sub_type in CORE_TYPES else ""
+                    )
+
+                    read_types += "\n        "
+                    read_types += "{} = TLObject.read(b{})\n        ".format(
+                        arg_name, f", {sub_type.title()}" if sub_type in CORE_TYPES else ""
+                    )
+                else:
+                    write_types += "\n        "
+                    write_types += f"b.write(self.{arg_name}.write())\n        "
+
+                    read_types += "\n        "
+                    read_types += f"{arg_name} = TLObject.read(b)\n        "
+
+        slots = ", ".join([f'"{i[0]}"' for i in sorted_args])
+        return_arguments = ", ".join([f"{i[0]}={i[0]}" for i in sorted_args])
+
+        directory = combinator.section
+
+        dir_path = DESTINATION_PATH / directory
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        module = combinator.namespace or '__init__'
+
+        with open(dir_path / f"{module}.py", "a") as f:
+            if directory + module not in header_inserted:
+                header_inserted.append(directory + module)
+                f.write(
+                    f"{notice}\n\n"
+                    f"{WARNING}\n\n"
+                    "from io import BytesIO\n\n"
+                    "from pyrogram.raw.core.primitives import Int, Long, Int128, Int256, Bool, Bytes, String, Double, Vector\n"
+                    "from pyrogram.raw.core import TLObject\n"
+                    "from pyrogram import raw\n"
+                    "from typing import List, Optional, Any\n\n"
+                )
+            f.write(combinator_tmpl.format(
+                name=combinator.name,
+                docstring=docstring,
+                slots=slots,
+                id=combinator.id,
+                qualname=f"{combinator.section}.{combinator.qualname}",
+                arguments=arguments,
+                fields=fields,
+                read_types=read_types,
+                write_types=write_types,
+                return_arguments=return_arguments
+            ))
+
+        d = namespaces_to_constructors if combinator.section == "types" else namespaces_to_functions
+
+        if combinator.namespace not in d:
+            d[combinator.namespace] = []
+
+        d[combinator.namespace].append(combinator.name)
+
+    for lists, namespaces in (
+            (namespaces_to_types, "base"),
+            (namespaces_to_constructors, "types"),
+            (namespaces_to_functions, "functions")
+    ):
+        with open(DESTINATION_PATH / namespaces / "__init__.py", "a") as f:
+            f.write(f"from . import {', '.join(filter(bool, lists))}")
+
+    with open(DESTINATION_PATH / "all.py", "w", encoding="utf-8") as f:
+        f.write(
+            f"{notice}\n\n"
+            f"{WARNING}\n\n"
+            f"from pyrogram.raw import core, {', '.join(set(c.section for c in combinators))}\n\n"
+            f"layer = {layer}\n\n"
+            "objects = {\n" +
+            "\n".join(tuple(
+                f'    {combinator.id.ljust(10)}: {combinator.section}.{combinator.qualname},' for combinator in
+                combinators
+            )) +
+            "\n    0xbc799737: core.BoolFalse,"
+            "\n    0x997275b5: core.BoolTrue,"
+            "\n    0x1cb5c415: core.Vector,"
+            "\n    0x73f1f8dc: core.MsgContainer,"
+            "\n    0xae500895: core.FutureSalts,"
+            "\n    0x0949d9dc: core.FutureSalt,"
+            "\n    0x3072cfa1: core.GzipPacked,"
+            "\n    0x5bb8e511: core.Message"
+            "\n}\n"
+        )
+
+
 if "__main__" == __name__:
     HOME_PATH = Path(".")
     DESTINATION_PATH = Path("../../pyrogram/raw")
     NOTICE_PATH = Path("../../NOTICE")
 
-    start(format=False)
+    (start if len(argv) > 1 and "orig" in argv[1].lower() else start_fast)(format=False)
